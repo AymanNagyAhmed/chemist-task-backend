50481f82a857d7fe8a0145ab04f1aee36e9d316f
AymanNagyAhmed <aymannagy.ahmed@gmail.com>
2024-12-01 06:29:08 +0400

    Refactor auth.module.ts to add JwtAuthGuard

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { ConfigService } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { AuthController } from '@/modules/auth/auth.controller';
 import { AuthService } from '@/modules/auth/auth.service';
 import { UsersModule } from '@/modules/users/users.module';
 import { JwtStrategy } from '@/modules/auth/strategies/jwt.strategy';
-import { LocalStrategy } from '@/modules/auth/strategies//local.strategy';
+import { LocalStrategy } from '@/modules/auth/strategies/local.strategy';
+import { JwtAuthGuard } from '@/modules/auth/guards/jwt.guard';
 
 @Module({
   imports: [
@@ -23,7 +25,15 @@ import { LocalStrategy } from '@/modules/auth/strategies//local.strategy';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    JwtStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
   exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
